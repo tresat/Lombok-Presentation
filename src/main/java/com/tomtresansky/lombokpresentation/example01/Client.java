9693d4f68c22de5d3b2f9ab4f24117ab378df913
Tom Tresansky <tom.tresansky@gmail.com>
2013-02-05 17:39:03 -0500

    Initial commit.
    -Set up Maven project, added Lombok dependency, verified it builds.

---
+++ src/main/java/com/tomtresansky/lombokpresentation/example01/Client.java
@@ -0,0 +1,8 @@
+package com.tomtresansky.lombokpresentation.example01;
+
+public class Client {
+  public static void main(final String[] args) {
+    final Getters g = new Getters();
+
+  }
+}
---
+++ src/main/java/com/tomtresansky/lombokpresentation/example01/Getters.java
@@ -0,0 +1,8 @@
+package com.tomtresansky.lombokpresentation.example01;
+
+import lombok.Getter;
+
+public class Getters {
+  @Getter
+  private String name;
+}
